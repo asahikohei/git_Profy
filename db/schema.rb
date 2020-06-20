6bc20c8889ddeb047cf1ecc2366a62e66476f6c6
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2020-06-20 06:50:23 +0000

    usersテーブル/groupsテーブルのアソシエーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_045825) do
+ActiveRecord::Schema.define(version: 2020_06_20_064730) do
+
+  create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "group_key"
+    t.text "detail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_06_20_045825) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "group_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
