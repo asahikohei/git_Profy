528c077040f4d7dab59ffae90b255869c23ecc8c
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2020-07-01 05:10:49 +0000

    topsコントローラーにグループに関係する質問全てが閲覧できるように設定

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,5 +1,7 @@
 class TopsController < ApplicationController
   def index
     @question = Question.new
+    question = Question.all
+    @questions = Question.where(text:"current_user.group_id == #{question}.group_id")
   end
 end
