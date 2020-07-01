20dc1e82eddc5c1559aa7e979481497981b87438
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2020-07-01 06:13:44 +0000

    question/answer/userモデル間のアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   attr_accessor :group_key
   belongs_to :group
   has_many :questions, ->{ order("created_at DESC") }
+  has_many :answers, ->{ order("updated_at DESC") }
   
   #validation
   before_validation :group_key_to_id, if: :has_group_key?
