ca8048e12161940024624a3bca1702f02a6eb62f
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2020-07-01 04:18:27 +0000

    ルーティング設定/questionsコントーラーcreateアクション

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "tops#index"
   resources :users, only: [:show, :edit, :update]
   resources :groups, only: [:show, :edit, :update]
+  resources :questions, only: :create
 end
