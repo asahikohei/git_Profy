91ae17caa52af980df6182bf26fcf50b1373e674
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2020-07-01 07:04:40 +0000

    answersコントローラーcreateアクションの設定

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,4 +4,14 @@ class AnswersController < ApplicationController
     @answer = Answer.new
     @answer.question_id = @question.id
   end
+  
+  def create
+    Answer.create(answer_params)
+    redirect_to :root and return
+  end
+  
+  private
+  def answer_params
+    params.require(:answer).permit(:text, :question_id).merge(user_id: current_user.id)
+  end
 end
