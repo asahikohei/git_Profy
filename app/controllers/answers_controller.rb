021b98f5e1290d6bc8839f824ede03c5c64bcdea
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2020-07-03 04:51:48 +0000

    answers/edit updateアクションへのルーティング設定とアクション定義

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -11,6 +11,16 @@ class AnswersController < ApplicationController
     @answer = Answer.create(answer_params)
   end
   
+  def edit
+    @answer = Answer.find(params[:id])
+  end
+  
+  def update
+    @answer = Answer.find(params[:id])
+    @question = @answer.question
+    @answer.update(update_params)
+  end
+  
   private
   def answer_params
     params.require(:answer).permit(:text, :question_id).merge(user_id: current_user.id)
@@ -20,5 +30,9 @@ class AnswersController < ApplicationController
     if Answer.exists?(question_id: params[:question_id], user_id: current_user.id)
       redirect_to :root
     end
+  end
+  
+  def update_params
+    params.require(:answer).permit(:text)
   end
 end
